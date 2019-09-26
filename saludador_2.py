1a07b6029a374051eff512e6adc8def2e82e5e7b
sergiopgomez <55547267+sergiopgomez@users.noreply.github.com>
2019-09-26 09:46:49 +0200

    Add files via upload

---
+++ saludador_2.py
@@ -0,0 +1,10 @@
+def saludador_2 ():
+    print "hola amigo" #Instruccion de salida 
+    nombre= raw_input("Como te llamas?: ") #instruccion de entrada
+    print "bienvenido a python"" "+nombre
+    
+   
+   
+    
+    
+saludador_2()
