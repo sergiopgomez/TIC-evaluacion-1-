6c198a21c9bc6b36fa45449bd2165356ecaa195c
sergiopgomez <55547267+sergiopgomez@users.noreply.github.com>
2019-10-15 12:09:08 +0200

    Add files via upload

---
+++ primos.py
@@ -0,0 +1,66 @@
+Python 2.5.4 (r254:67916, Dec 23 2008, 15:19:34) [MSC v.1400 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+
+    ****************************************************************
+    Personal firewall software may warn about the connection IDLE
+    makes to its subprocess using this computer's internal loopback
+    interface.  This connection is not visible on any external
+    interface and no data is sent to or received from the Internet.
+    ****************************************************************
+    
+IDLE 1.2.4      
+>>> ================================ RESTART ================================
+>>> 
+say me your number13
+no es primo
+>>> ================================ RESTART ================================
+>>> 
+say me your number13
+no es primo
+>>> ================================ RESTART ================================
+>>> 
+say me your number14
+es primo
+>>> ================================ RESTART ================================
+>>> 
+say me your number13
+es primo
+>>> ================================ RESTART ================================
+>>> 
+say me your number13
+es primo
+>>> ================================ RESTART ================================
+>>> 
+say me your number14.
+
+Warning (from warnings module):
+  File "C:/Python25/primos-py", line 4
+    for i in range (2,numero):
+DeprecationWarning: integer argument expected, got float
+
+Traceback (most recent call last):
+  File "C:/Python25/primos-py", line 11, in <module>
+    primo()
+  File "C:/Python25/primos-py", line 5, in primo
+    if(numero&i==0):
+TypeError: unsupported operand type(s) for &: 'float' and 'int'
+>>> ================================ RESTART ================================
+>>> 
+say me your number
+14
+es primo
+>>> ================================ RESTART ================================
+>>> 
+>>> 
+say me your number20
+no es primo
+>>> ================================ RESTART ================================
+>>> 
+say me your number
+3
+es primo
+>>> ================================ RESTART ================================
+>>> 
+say me your number13
+no es primo
+>>> 
