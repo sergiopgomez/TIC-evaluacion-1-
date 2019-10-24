798507716993a39dad7f8b7eb8ee6850be011f3a
sergiopgomez <55547267+sergiopgomez@users.noreply.github.com>
2019-10-24 09:19:47 +0200

    Add files via upload

---
+++ multiplicacion1.py
@@ -0,0 +1,34 @@
+def multiplicacion ():
+    x=0
+    while(x==0):
+        x=input ("Introduzca el primer numero: ")
+        if (x==0):
+            print "Error has introducido un 0"
+    y=0
+    while(y==0):
+        y=input("introduzca el segundo digito que quiera multiplicar: ")
+        if(y==0):
+            print "Error has introducido un 0"
+        
+    z=0
+    while(z==0):
+        z=input ("Introduzca el tercer numero: ")
+        if (z==0):
+            print "Error has introducido un 0"
+    a=0
+    while(a==0):
+        a=input ("Introduzca el cuarto numero: ")
+        if (a==0):
+            print "Error has introducido un 0"
+        
+        
+        
+    
+    else:
+        resultado=x*y*z*a
+        print resultado
+        print "ya hemos terminado de contar espero que le haya ayudado a resolver su duda"
+
+multiplicacion()
+
+    
