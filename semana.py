b74c489b5a5c8cb668b28e00710e7145a123659d
sergiopgomez <55547267+sergiopgomez@users.noreply.github.com>
2019-10-25 09:32:44 +0200

    Add files via upload

---
+++ semana.py
@@ -0,0 +1,22 @@
+def dias_de_la_semana():
+    x=input("pon un numero del 1 al 7: ")
+    if(x==1):
+        print"lunes"
+    if(x==2):
+        print"martes"
+
+    if(x==3):
+        print"miercoles"
+    if(x==4):
+        print"jueves"
+    if(x==5):
+        print"viernes"
+    if(x==6):
+        print"sabado"
+    if(x==7):
+        print"domingo"
+    if(x>=8):
+        print "no hay tantos dias subnormal"
+    
+
+dias_de_la_semana()
