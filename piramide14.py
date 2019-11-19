5638eaa74f59e6bbaf2dfa74e23c3348fcdc51e9
sergiopgomez <55547267+sergiopgomez@users.noreply.github.com>
2019-11-19 11:54:19 +0100

    Add files via upload

---
+++ piramide14.py
@@ -0,0 +1,8 @@
+def ejercicio_14():
+    h= input("introduzca la altura que quieras")
+    for filas in range (1,h+1):
+        for col in range (1,2*h):
+            print "*"
+   
+
+ejercicio_14()
